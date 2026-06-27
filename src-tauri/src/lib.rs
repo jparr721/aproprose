0b463069e193feeca3c4d2c912a385e40db214f4
Jarred Parr <jparr721@gmail.com>
2026-06-26 20:50:12 -0600

    feat(updater): enable tauri updater + process plugins and config

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -348,6 +348,8 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![
             open_project,
             create_project,
